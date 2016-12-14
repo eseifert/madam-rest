2f8a2fdad8deb96b7b3c971baf866f248c23fdda
Erich Seifert <dev@erichseifert.de>
2016-12-14 09:35:22 +0100

    Improve serialization of asset metadata.

--- madam_rest/views.py
+++ madam_rest/views.py
@@ -1,6 +1,27 @@
+from datetime import datetime
 from flask import jsonify, url_for
+from fractions import Fraction
+from frozendict import frozendict
 
 from madam_rest import app, asset_storage
+
+
+def _serializable(value):
+    """
+    Utility function to convert data structures with immutable types to
+    mutable, serializable data structures.
+    :param value: data structure with immutable types
+    :return: mutable, serializable data structure
+    """
+    if isinstance(value, (tuple, set, frozenset)):
+        return [_serializable(v) for v in value]
+    elif isinstance(value, frozendict):
+        return {k: _serializable(v) for k, v in value.items()}
+    elif isinstance(value, datetime):
+        return value.isoformat()
+    elif isinstance(value, Fraction):
+        return float(value)
+    return value
 
 
 @app.route('/assets/')
@@ -21,5 +42,5 @@ def asset_retrieve(asset_key):
         "links": {
             "self": url_for(asset_retrieve, asset_key=asset_key)
         },
-        "meta": {}  # TODO: _mutable(asset.metadata)
+        "meta": _serializable(asset.metadata)
     })
